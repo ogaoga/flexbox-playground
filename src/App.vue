4ef6342008c1e90f05ea973bc742faba81ba460e
ogaoga <tsutomu.ogasawara+github@gmail.com>
2019-09-20 00:57:54 +0900

    Added <Box> but does not work correctly. (wip)

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,46 @@
   <v-app>
     <v-app-bar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span class="font-weight-light">Flexbox Playground</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
+      <v-btn 
         text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+        @click.stop="drawer = !drawer">        
+        <v-icon>mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <v-navigation-drawer 
+        v-model="drawer"
+        absolute temporary right>
+        <template v-slot:prepend>
+          
+        </template>
+      </v-navigation-drawer>
+
+      <v-container fluid class="d-flex">
+        <div v-for="(box, i) in boxes" :key="i">
+          <Box parameters={box}>{{i}}</Box>
+        </div>
+      </v-container>
+
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Box from './components/Box.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld,
+    Box,
   },
   data: () => ({
-    //
+    drawer: false,
+    boxes: [1, 2, 3, [4, 5, 6]],
   }),
 });
 </script>
---
+++ src/components/Box.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card class="box">
+    <v-card-text>
+      <Box v-if="Array.isArray(parameters)" parameters="parameters" />
+      <span v-else><slot /></span>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  props: {
+    parameters: Number,
+  },
+  data: () => ({}),
+});
+</script>
+
+<style lang="scss" scoped>
+.box {
+  margin: 4px;
+}
+</style>
